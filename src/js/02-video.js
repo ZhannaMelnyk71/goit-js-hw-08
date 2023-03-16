8cc491e867421e90e472739ba1d21426a261ee9c
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-03-16 21:20:33 +0100

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timeKey ='videoplayer-current-time'     
+const playTime = localStorage.getItem(timeKey) ? localStorage.getItem(timeKey):0;
+
+function getCurrentTime(event) {
+    localStorage.setItem(timeKey, event.seconds);
+ }
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+    
+player.setCurrentTime(playTime)
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
 
