4b7f2b0ef4de010008d5e7a2b64bcf98a6ec0ce0
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-03-16 18:59:36 +0100

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,40 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup (items){
+    const markup = items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>`;
+    }).join('');
+
+    return markup;
+} 
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData : "alt",
+    captionDelay : 250
+});
+
+gallery.on('show.simplelightbox', function () {
+});
